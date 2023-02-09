8bafce362fa92d3a656598bac353ce88b2cdd00e
Georgii Pimkin <geo.pimkin@gmail.com>
2023-02-09 23:57:21 +0700

    create tTask kameleoon prj

---
+++ src/main/java/org/tTask/ApiJApplication.java
@@ -0,0 +1,13 @@
+package org.tTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/tTask/ApiJApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
